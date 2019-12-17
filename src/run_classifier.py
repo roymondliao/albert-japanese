ce90636a517b6a34f39acc20859f69c893d4e0d4
Jun-ya Norimatsu <dummy@alinear.jp>
2019-12-18 01:45:55 +0900

    Fix run_classifier

--- src/run_classifier.py
+++ src/run_classifier.py
@@ -190,37 +190,7 @@ class InputFeatures(object):
     self.is_real_example = is_real_example
 
 
-class DataProcessor(object):
-  """Base class for data converters for sequence classification data sets."""
-
-  def get_train_examples(self, data_dir):
-    """Gets a collection of `InputExample`s for the train set."""
-    raise NotImplementedError()
-
-  def get_dev_examples(self, data_dir):
-    """Gets a collection of `InputExample`s for the dev set."""
-    raise NotImplementedError()
-
-  def get_test_examples(self, data_dir):
-    """Gets a collection of `InputExample`s for prediction."""
-    raise NotImplementedError()
-
-  def get_labels(self):
-    """Gets the list of labels for this data set."""
-    raise NotImplementedError()
-
-  @classmethod
-  def _read_tsv(cls, input_file, quotechar=None):
-    """Reads a tab separated value file."""
-    with tf.gfile.Open(input_file, "r") as f:
-      reader = csv.reader(f, delimiter="\t", quotechar=quotechar)
-      lines = []
-      for line in reader:
-        lines.append(line)
-      return lines
-
-
-class LivedoorProcessor(DataProcessor):
+class LivedoorProcessor(classifier_utils.DataProcessor):
   """Processor for the livedoor data set (see https://www.rondhuit.com/download.html)."""
 
   def get_train_examples(self, data_dir):
