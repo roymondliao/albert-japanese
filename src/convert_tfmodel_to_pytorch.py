1d38fe075a0e8fe48d9dfa35730ff0b92c22875d
Jun-ya Norimatsu <dummy@alinear.jp>
2020-03-20 20:53:36 +0900

    Add a convert script.

---
+++ src/convert_tfmodel_to_pytorch.py
@@ -0,0 +1,29 @@
+import json
+import logging
+
+from transformers import AlbertConfig, AlbertForMaskedLM, load_tf_weights_in_albert
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def main(args):
+    with open(args.config) as fp:
+        data = json.loads(fp.read())
+    config = AlbertConfig(**data)
+    model = AlbertForMaskedLM(config)
+    model: AlbertForMaskedLM = load_tf_weights_in_albert(model, config, args.checkpoint)
+    model.albert
+    model.save_pretrained(args.output)
+
+
+def get_parser():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config")
+    parser.add_argument("checkpoint")
+    parser.add_argument("output")
+    return parser
+
+
+if __name__ == "__main__":
+    main(get_parser().parse_args())
