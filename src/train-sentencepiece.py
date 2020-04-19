46707b5d9752a54d1870bb8de9ee16eeb8b5e4cb
Jun-ya Norimatsu <dummy@alinear.jp>
2020-04-19 17:35:21 +0900

    Update sentencepiece training command

--- src/train-sentencepiece.py
+++ src/train-sentencepiece.py
@@ -24,7 +24,11 @@ def _get_text_file(text_dir=TEXTDIR):
 
 def train(prefix=PREFIX, vocab_size=VOCABSIZE, ctl_symbols=CTLSYMBOLS):
     files = _get_text_file()
-    command = f'--input={files} --model_prefix={prefix} --vocab_size={vocab_size} --control_symbols={ctl_symbols} --input_sentence_size=15000000 --shuffle_input_sentence=true'
+    # https://github.com/google-research/albert/blob/a41cf11700c1ed2b7beab0a2649817fa52c8d6e1/README.md#sentencepiece
+    command = f'--input={files} --model_prefix={prefix} --vocab_size={vocab_size} ' \
+              f'--pad_id=0 --unk_id=1 --eos_id=-1 --bos_id=-1 --user_defined_symbols=(,),",-,.,–,£,€ ' \
+              f'--control_symbols={ctl_symbols} --input_sentence_size=15000000 ' \
+              f'--shuffle_input_sentence=true --character_coverage=0.99995 --model_type=unigram'
     sp.SentencePieceTrainer.Train(command)
 
 
